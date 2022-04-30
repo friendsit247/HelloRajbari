9018fbf8a7feda4dacb29fb9594cea54e7e36a74
Daud Hoshen Rimon <daudhrimon@gmail.com>
2022-04-30 14:04:06 +0600

    Rimon 29-05-2022

--- app/src/main/java/com/friendsit/hellorajbari/MainActivity.java
+++ app/src/main/java/com/friendsit/hellorajbari/MainActivity.java
@@ -1,29 +1,99 @@
 package com.friendsit.hellorajbari;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private SearchView homeSv;
     public static DatabaseReference databaseReference;
+    private List<HelloModel> list;
+    private LinearLayout btnLay;
+    private RecyclerView homeRecycler;
+    private HelloAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initial();
+
+        getAllData();
+
+        homeSv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) { return false; }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                homeSearchMethod(newText);
+                return true;
+            }
+        });
     }
 
-    private void initial() {
-        homeSv = findViewById(R.id.homeSv);
-        databaseReference = FirebaseDatabase.getInstance().getReference("AllData");
-        databaseReference.keepSynced(true);
+    private void homeSearchMethod(String newText) {
+        List<HelloModel> searchList = new ArrayList<>();
+        if (!newText.isEmpty()){
+            searchList.clear();
+            for (int i=0; i < list.size(); i++){
+                if (list.get(i).getNam().toLowerCase(Locale.ROOT).contains(newText)||
+                        list.get(i).getTit().toLowerCase(Locale.ROOT).contains(newText)||
+                        list.get(i).getPho().contains(newText)){
+                    searchList.add(list.get(i));
+                }
+            }
+            adapter = new HelloAdapter(MainActivity.this,searchList);
+            btnLay.setVisibility(View.GONE);
+            homeRecycler.setVisibility(View.VISIBLE);
+            homeRecycler.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        }else{
+            homeRecycler.setVisibility(View.GONE);
+            btnLay.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void getAllData() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    list.clear();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                        if (dataSnapshot.exists()){
+                            for (DataSnapshot eachSnap : dataSnapshot.getChildren()){
+                                if (eachSnap.exists()){
+                                    HelloModel helloModel = eachSnap.getValue(HelloModel.class);
+                                    list.add(helloModel);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        });
+
     }
 
     public void govtOfficeClick(View view) {
@@ -96,5 +166,15 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this,TaskActivity.class);
         intent.putExtra("State","About");
         startActivity(intent);
+    }
+
+    private void initial() {
+        homeSv = findViewById(R.id.homeSv);
+        databaseReference = FirebaseDatabase.getInstance().getReference("AllData");
+        databaseReference.keepSynced(true);
+        list = new ArrayList<>();
+        homeRecycler = findViewById(R.id.homeRecycler);
+        btnLay = findViewById(R.id.btnLay);
+        homeRecycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
     }
 }
